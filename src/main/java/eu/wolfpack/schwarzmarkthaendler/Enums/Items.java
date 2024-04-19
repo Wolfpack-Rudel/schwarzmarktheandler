7e98e211db96ed46d50c9111378fd77267a2a4a5
LouiLetsPlayPro <51512114+LouiLetsPlayPro@users.noreply.github.com>
2024-04-20 00:52:04 +0200

    Create Randomizer for Quests + GUI Items

--- src/main/java/eu/wolfpack/schwarzmarkthaendler/Enums/Items.java
+++ src/main/java/eu/wolfpack/schwarzmarkthaendler/Enums/Items.java
@@ -11,7 +11,20 @@ public enum Items {
     PLACEHOLDER(GRAY_STAINED_GLASS_PANE,  ChatColor.translateAlternateColorCodes('&', "&l "), 10100, " ", " "),
     SHOP(ORANGE_CONCRETE, ChatColor.translateAlternateColorCodes('&', "&b&lShop"), 10101),
     QUEST(YELLOW_CONCRETE, ChatColor.translateAlternateColorCodes('&', "&b&lQuests"), 10102),
-    BACK(PAPER, ChatColor.translateAlternateColorCodes('&', "&c&lZurück"), 10003);
+    BACK(PAPER, ChatColor.translateAlternateColorCodes('&', "&c&lZurück"), 10003),
+    COMPLETE_20(LIME_STAINED_GLASS_PANE, "20%", 10110),
+    COMPLETE_40(LIME_STAINED_GLASS_PANE, "40%", 10111),
+    COMPLETE_60(LIME_STAINED_GLASS_PANE, "60%", 10112),
+    COMPLETE_80(LIME_STAINED_GLASS_PANE, "80%", 10113),
+    COMPLETE_100(LIME_STAINED_GLASS_PANE, "100%", 10114),
+    INCOMPLETE_20(RED_STAINED_GLASS_PANE, "20%", 10120),
+    INCOMPLETE_40(RED_STAINED_GLASS_PANE, "40%", 10120),
+    INCOMPLETE_60(RED_STAINED_GLASS_PANE, "60%", 10120),
+    INCOMPLETE_80(RED_STAINED_GLASS_PANE, "80%", 10120),
+    INCOMPLETE_100(RED_STAINED_GLASS_PANE, "100%", 10120),
+
+
+    ;
 
 
     private Material material;
---
+++ src/main/java/eu/wolfpack/schwarzmarkthaendler/utils/PlayQuests.java
@@ -0,0 +1,38 @@
+package eu.wolfpack.schwarzmarkthaendler.utils;
+
+public class PlayQuests {
+
+    private PlayerQuest pq1;
+    private PlayerQuest pq2;
+    private PlayerQuest pq3;
+
+    public PlayQuests(PlayerQuest pq1, PlayerQuest pq2, PlayerQuest pq3) {
+        this.pq1 = pq1;
+        this.pq2 = pq2;
+        this.pq3 = pq3;
+    }
+
+    public PlayerQuest getPq1() {
+        return pq1;
+    }
+
+    public void setPq1(PlayerQuest pq1) {
+        this.pq1 = pq1;
+    }
+
+    public PlayerQuest getPq2() {
+        return pq2;
+    }
+
+    public void setPq2(PlayerQuest pq2) {
+        this.pq2 = pq2;
+    }
+
+    public PlayerQuest getPq3() {
+        return pq3;
+    }
+
+    public void setPq3(PlayerQuest pq3) {
+        this.pq3 = pq3;
+    }
+}
